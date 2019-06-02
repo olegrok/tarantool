09876a7c68193ca0f277053ac1b85f71664c1dd1
Oleg Babin <babinoleg@mail.ru>
2019-06-02 21:26:34 +0300

    Add test for is_relevant

--- test/unit/zcurve.cc
+++ test/unit/zcurve.cc
@@ -4,22 +4,24 @@
 
 const size_t key_size_in_bits = 64lu;
 
-static z_address *create_key2d_from_number(uint64_t num) {
+static z_address*
+create_key2d_from_number(uint64_t num) {
 	z_address *key = bit_array_create(2 * key_size_in_bits);
 	bit_array_clear_all(key);
 	bit_array_add_uint64(key, num);
 	return key;
 }
 
-static void next_jump_in_check_2d() {
+void
+next_jump_in_check_2d() {
 	size_t test_plan = 19;
 	plan(2 * test_plan);
-	header();
-	struct test_case {
-		uint64_t test_point;
-		uint64_t expected;
-	};
+    struct test_case {
+        uint64_t test_point;
+        uint64_t expected;
+    };
 
+	header();
 	uint64_t lower_bound = 11;
 	uint64_t upper_bound = 50;
 	struct test_case test_cases[] {
@@ -66,10 +68,59 @@ static void next_jump_in_check_2d() {
 	bit_array_free(z_lower_bound);
 	bit_array_free(z_upper_bound);
 	footer();
+    check_plan();
+}
+
+void
+is_relevant_check_2d() {
+    size_t test_plan = 14;
+    header();
+    plan(test_plan);
+
+    struct test_case {
+        uint64_t test_point;
+        bool expected;
+    };
+
+    uint64_t lower_bound = 4;
+    uint64_t upper_bound = 51;
+    struct test_case test_cases[] {
+            { 0, false },
+            { 4, true },
+            { 7, true },
+            { 8, false },
+            { 11, false },
+            { 12, true },
+            { 19, true },
+            { 20, false },
+            { 23, false },
+            { 24, true },
+            { 35, false },
+            { 47, false },
+            { 51, true },
+            { 52, false },
+    };
+
+    z_address *z_lower_bound = create_key2d_from_number(lower_bound);
+    z_address *z_upper_bound = create_key2d_from_number(upper_bound);
+
+    for (size_t i = 0; i < test_plan; ++i) {
+        z_address *value = create_key2d_from_number(test_cases[i].test_point);
+        bool is_relevant = z_value_is_relevant(value, z_lower_bound,
+                z_upper_bound);
+        is(is_relevant, test_cases[i].expected,
+                is_relevant ? "in query box" : "not in query box");
+        bit_array_free(value);
+    }
+
+    bit_array_free(z_lower_bound);
+    bit_array_free(z_upper_bound);
+    footer();
+    check_plan();
 }
 
 int main() {
-	plan(1);
 	next_jump_in_check_2d();
+    is_relevant_check_2d();
 	return 0;
 }
