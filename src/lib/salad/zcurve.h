1c60587f7ebc404aeb4607b5efddd08fe8f3a005
Babin Oleg <babinoleg@mail.ru>
2019-06-02 12:39:47 +0300

    Fix segfault. Remove obsolete function

--- src/lib/salad/zcurve.h
+++ src/lib/salad/zcurve.h
@@ -16,6 +16,12 @@ z_address* zeros(uint32_t part_count);
 z_address* ones(uint32_t part_count);
 z_address* get_next_zvalue(const z_address* z_value, const z_address* lower_bound,
 						   const z_address* upper_bound, bool *in_query_box);
+
+/*
+ * Provided a minimum Z-address, a maximum Z-address, and a test Z-address,
+ * the isRelevant function tells us whether the test address falls within
+ * the query rectangle created by the minimum and maximum Z-addresses.
+ */
 bool z_value_is_relevant(const z_address* z_value, const z_address* lower_bound,
 						 const z_address* upper_bound);
 
