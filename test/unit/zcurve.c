e4b97b73c3fbd5c6c16e5a091579f07f93130d09
Oleg Babin <babinoleg@mail.ru>
2020-02-23 16:56:06 +0300

    test: add test for zcurve

--- test/unit/zcurve.c
+++ test/unit/zcurve.c
@@ -41,19 +41,6 @@ next_jump_in_check_2d() {
 			{ 34, 35 },
 			{ 40, 48 },
 			{ 49, 50 },
-#ifndef NDEBUG
-/*
- * For performance reasons border points are disabled
- * in not debug mode.
- * For details read a comment in get_next_zvalue function.
- */
-			{ 11, 11 },
-			{ 14, 14 },
-			{ 26, 26 },
-			{ 33, 33 },
-			{ 35, 35 },
-			{ 50, 50 },
-#endif
 	};
 	size_t test_plan = sizeof(test_cases) / sizeof(struct test_case);
 	plan(test_plan);
@@ -85,7 +72,8 @@ next_jump_in_check_2d() {
 }
 
 static void
-is_relevant_check_2d() {
+is_relevant_check_2d()
+{
 	word_size_t dim = 2;
 	struct mempool pool = {};
 	mempool_create(&pool, cord_slab_cache(),
@@ -142,6 +130,120 @@ is_relevant_check_2d() {
 }
 
 static void
+is_relevant_check_2d_equal()
+{
+	const word_size_t dim = 2;
+	struct mempool pool = {};
+	mempool_create(&pool, cord_slab_cache(), bit_array_bsize(dim));
+
+	struct test_case {
+		uint64_t point[dim];
+		bool expected;
+	};
+
+	/* Representation of number + integer */
+	struct test_case test_cases[] = {
+			/* {2, 3} */
+			{{0, 17293823118858518528ULL}, false},
+			/* {3, 4} */
+			{{0, 17293825043003867136ULL}, true},
+			/* {3, 5} */
+			{{0, 17293823118858518528ULL}, false},
+	};
+
+	size_t test_plan = sizeof(test_cases)/ sizeof(struct test_case);
+	header();
+	plan(test_plan);
+
+	z_address *lower_bound = z_value_create(&pool, dim);
+	lower_bound[0] = 0;
+	lower_bound[1] = 17293825043003867136ULL;
+	z_address *upper_bound = z_value_create(&pool, dim);
+	upper_bound[0] = 0;
+	upper_bound[1] = 17293825043003867136ULL;
+
+	z_address *value = z_value_create(&pool, dim);
+	for (size_t i = 0; i < test_plan; ++i) {
+		value[0] = test_cases[i].point[0];
+		value[1] = test_cases[i].point[1];
+		bool is_relevant = z_value_is_relevant(value, lower_bound,
+				upper_bound, dim);
+		is(is_relevant, test_cases[i].expected,
+		   is_relevant ? "in query box" : "not in query box");
+	}
+
+	bit_array_free(&pool, value);
+	bit_array_free(&pool, lower_bound);
+	bit_array_free(&pool, upper_bound);
+	mempool_destroy(&pool);
+	footer();
+	check_plan();
+}
+
+static void
+is_relevant_check_3d()
+{
+	const word_size_t dim = 3;
+	struct mempool pool = {};
+	mempool_create(&pool, cord_slab_cache(),
+				   bit_array_bsize(dim));
+	header();
+	struct test_case {
+		uint64_t point[dim];
+		bool expected;
+	};
+	struct test_case test_cases[] = {
+			{{0, 1, 1}, false},
+			{{1, 0, 1}, false},
+			{{1, 1, 0}, false},
+			{{5, 5, 5}, true},
+			{{1, 1, 1}, true},
+			{{1, 2, 3}, true},
+			{{3, 4, 5}, true},
+			{{6, 7, 8}, true},
+			{{9, 10, 11}, false},
+			{{9, 9, 9}, true},
+			{{10, 9, 9}, false},
+			{{9, 10, 9}, false},
+			{{9, 9, 10}, false},
+			{{15, 9, 10}, false},
+			{{5, 15, 5}, false},
+			{{4, 4, 20}, false},
+	};
+	size_t test_plan = sizeof(test_cases)/ sizeof(struct test_case);
+	plan(test_plan);
+
+	struct bit_array_interleave_lookup_table *table =
+			bit_array_interleave_new_lookup_tables(&pool, dim);
+	uint64_t lower_bound_point[] = {1, 1, 1};
+	uint64_t upper_bound_point[] = {9, 9, 9};
+
+	bit_array *lower_bound = z_value_create(&pool, dim);
+	bit_array_interleave(table, lower_bound_point, lower_bound);
+	bit_array *upper_bound = z_value_create(&pool, dim);
+	bit_array_interleave(table, upper_bound_point, upper_bound);
+
+	bit_array *test_point = z_value_create(&pool, dim);
+
+	for (size_t i = 0; i < test_plan; i++) {
+		bit_array_interleave(table, test_cases[i].point, test_point);
+		bool is_relevant = z_value_is_relevant(test_point, lower_bound,
+											   upper_bound, dim);
+		is(is_relevant, test_cases[i].expected,
+		   is_relevant ? "in query box" : "not in query box");
+		bit_array_clear_all(test_point, dim);
+	}
+
+	z_value_free(&pool, test_point);
+	z_value_free(&pool, lower_bound);
+	z_value_free(&pool, upper_bound);
+	bit_array_interleave_free_lookup_tables(table);
+	mempool_destroy(&pool);
+	footer();
+	check_plan();
+}
+
+static void
 zcurve_C_test_init()
 {
 	/* Suppress info messages. */
@@ -155,6 +257,8 @@ int main() {
 	zcurve_C_test_init();
 
 	next_jump_in_check_2d();
-    is_relevant_check_2d();
+	is_relevant_check_2d();
+	is_relevant_check_2d_equal();
+	is_relevant_check_3d();
 	return 0;
 }
